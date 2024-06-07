a91edc51ab03d6d5dd5b3e1cf2d9882c294591f3
Zoha Nafees <zohanafees09@gmail.com>
2024-06-08 00:28:27 +0500

    Ts task 8

---
+++ main.js
@@ -0,0 +1,7 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Author name=Zoha Nafees
+//7 june 2024
+var favourite_Number = 13;
+//consoling A favourite Number
+console.log(favourite_Number);
+export {};
